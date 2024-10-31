432378809e399e8005ed901a9c0db91cd008a63f
ineguiluz1 <i.eguiluz@opendeusto.es>
2024-10-31 12:55:15 +0100

    AuthController

--- src/main/java/Strava/facade/AuthController.java
+++ src/main/java/Strava/facade/AuthController.java
@@ -1,4 +1,22 @@
 package Strava.facade;
+import Strava.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/auth")
 
 public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {this.authService = authService;}
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(String email, String password)
+    {
+        Optional<String> token = authService.login()
+    }
 }
