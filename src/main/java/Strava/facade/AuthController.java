cce3c6ad1c0be42da048dd534ef0c23e0f858aa3
aitordiezUD <aitor.diez@opendeusto.es>
2024-10-31 12:50:46 +0100

    Import Swagger

--- src/main/java/Strava/facade/AuthController.java
+++ src/main/java/Strava/facade/AuthController.java
@@ -1,4 +1,11 @@
 package Strava.facade;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 public class AuthController {
+
+
 }
